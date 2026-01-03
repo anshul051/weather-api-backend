c7c63b395f9025beb12b129718a719466c24856b
anshul051 <anshulkumararya51@gmail.com>
2026-01-04 00:39:32 +0530

    first commit with initial files setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gradient-to-r from-indigo-300 via-purple-500 to-pink-300 flex justify-center items-center'>
+      <div className='h-[30rem] w-[25rem] bg-white flex flex-col rounded-2xl overflow-hidden shadow-lg '>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
