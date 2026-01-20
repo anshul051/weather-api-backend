8144cd7f55ac17cd768b92fbdf4562ead3fce3c0
anshul051 <anshulkumararya51@gmail.com>
2026-01-21 00:42:22 +0530

    updtaed backend CORS

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[
+      "http://localhost:5173",
+      "https://weather-api-frontend.onrender.com"
+    ],
   })
 );
 
